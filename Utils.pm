b8d36f4b27d5804321c1c643ed7612a45b856ed1
gosha92 <alexeew.georgiy@gmail.com>
2014-12-20 02:08:28 +0500

    ready

---
+++ Utils.pm
@@ -0,0 +1,113 @@
+package Utils;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT = qw(diskRead parse hexdump DUMP);  # symbols to export on request
+
+use strict;
+use warnings;
+no warnings 'portable';
+binmode(STDOUT,':utf8');
+use Time::Piece;
+use Encode qw(decode);
+use Data::Dumper;
+use Data::HexDump;
+$/ = undef;
+use utf8;
+
+# Читает 512 или length мегабайтов, начиная со смещения offset
+# diskread(offset, [length])
+sub diskRead {
+	my $offset = int($_[0]);
+	my $skip = int($offset / (1024*1024));
+	my $s = $offset % (1024*1024);
+	system "dd if=\\\\.\\C: skip=${skip} count=1 bs=1M 2>nul > dd.bin";
+	open my $f, 'dd.bin';
+	binmode $f;
+	my $b = <$f>;
+	close $f;
+	my $length = $_[1] // 512;
+	return substr $b, $s, $length;
+}
+
+# Упаковывает байты
+# parse(data, offset, length, [template])
+sub parse {
+	my ($data, $offset, $length, $template) = (@_, '');
+	if ($template eq 'unicode') {
+		return decode("UTF-16LE", substr($data, $offset, $length));
+	} elsif ($template eq 'flags') {
+		my $flags = unpack "V*", substr($data, $offset, $length);
+		my @fnames = ('read-only','hidden','system','','','archive',
+		'device','normal','temporary','sparse','reparse point',
+		'compressed','offline','not indexed','encrypted');
+		return join ", ", @fnames[ grep {$flags & (1<<$_)} 0..31 ];
+	} elsif ($template eq 'variable') {
+		my $bytes = join '', reverse(split(//, substr($data, $offset, $length)));
+		$bytes =~ s/^\0+//;
+		return hex(unpack "H*", $bytes);
+	} elsif ($length == 1) {
+		$template = "c*";
+	} elsif ($length == 2) {
+		$template = "v*";
+	} elsif ($length == 4) {
+		$template = "V*";
+	} elsif ($length == 8) {
+		my $time = unpack "Q*", substr($data, $offset, $length);
+		$time = int( ($time - 116444736000000000) / 10000000 );
+		return localtime($time)->strftime('%d.%m.%Y %H:%M:%S');
+	}
+	unpack $template, substr($data, $offset, $length);
+}
+
+# Int -> 0xHex
+sub HEXED { '0x' . uc sprintf("%x", shift) };
+
+sub hexdump {
+	substr HexDump(substr shift, 0, 64), 79;
+}
+
+my %attr_names = (
+	0x10, '$STANDARD_INFORMATION',
+	0x20, '$ATTRIBUTE_LIST',
+	0x30, '$FILE_NAME',
+	0x40, '$VOLUME_VERSION',
+	0x40, '$OBJECT_ID',
+	0x50, '$SECURITY_DESCRIPTOR',
+	0x60, '$VOLUME_NAME',
+	0x70, '$VOLUME_INFORMATION',
+	0x80, '$DATA',
+	0x90, '$INDEX_ROOT',
+	0xA0, '$INDEX_ALLOCATION',
+	0xB0, '$BITMAP',
+	0xC0, '$SYMBOLIC_LINK',
+	0xC0, '$REPARSE_POINT',
+	0xD0, '$EA_INFORMATION',
+	0xE0, '$EA',
+	0xF0, '$PROPERTY_SET',
+	0x100, '$LOGGED_UTILITY_STREAM'
+);
+
+sub DUMP(\@) {
+	print "\n\n";
+	my @attributes = @{shift;};
+	for my $a (@attributes) {
+		my $name = $attr_names{$a->{type}} // HEXED($a->{type});
+		my $resident = $a->{resident} ? "NONRESIDENT" : "RESIDENT";
+		print "$name\n$resident";
+		if ($a->{resident}) {
+			my @dataruns = @{$a->{dataruns}};
+			print ' [';
+			print HEXED(shift @dataruns) . '-' . HEXED(shift @dataruns);
+			while(@dataruns) { print ', ' . HEXED(shift @dataruns) . '-' . HEXED(shift @dataruns) };
+			print ']';
+		}
+		print "\n";
+		if ($a->{resident}) {
+			print $a->{data} . "\n";
+		} else {
+			print $a->{data} . "\n\n";
+		}
+	}
+}
+
+1;
--- parser.pl
+++ parser.pl
@@ -1,75 +1,74 @@
 use strict;
 use warnings;
 no warnings 'portable';
-use Time::Piece;
+
+use Utils;
 
 # Usage:
 my $hex = shift // '';
 die "\n    USAGE: attr.pl HEX_OFFSET_TO_FILE_RECORD\n" unless $hex =~ /^(0x)?[0-9a-f]+$/i;
 
-# Читает 512 байтов, начиная со смещения <skip>*512 байтов
-# diskread(skip)
-sub diskRead($) {
-	my $skip = int($_[0]);
-	`dd if=\\\\.\\C: skip=${skip} count=1 bs=512 2>nul`;
-}
-
-# Упаковывает байты
-# parse(data, offset, length, [template])
-sub parse {
-	my ($data, $offset, $length, $template) = @_;
-	unpack $template, substr($data, $offset, $length);
-}
-
-# Int -> 0xHex
-sub HEXED { '0x' . uc sprintf("%x", shift) };
-
-
-
-
-
-
-
-
-
-# Извлекаем файловую запись
-my $file_record = diskRead( hex($hex) / 512 );
+# Извлекаем файловую запись по указанному смещению
+my $file_record = diskRead( hex($hex) );
 
 # Проверяем сигнатуру 'FILE'
-my $signature = substr $file_record, 0, 4;
-die "\n    Missed 'FILE' signature!\n" unless $signature eq "FILE";
+die "\n    Missed 'FILE' signature!\n" unless substr($file_record, 0, 4) eq "FILE";
 
 # Вычисляем смещение до атрибутов
-my $offset = parse($file_record, 0x14, 2, "v*");
-my $file_record_length = parse($file_record, 0x18, 4, "V*");
+my $offset = parse($file_record, 0x14, 2);
+my $file_record_length = parse($file_record, 0x18, 4);
 
 # Парсим атрибуты
+my @attributes = ();
 while ($offset < $file_record_length - 8) {
-	my $type = parse($file_record, $offset + 0x00, 4, "V*");
-	my $length = parse($file_record, $offset + 0x04, 4, "V*");
-	my $resident = parse($file_record, $offset + 0x08, 1, "c*");
-	my $data_offset = parse($file_record, $offset + 0x14, 2, "v*");
+	my $type =        parse($file_record, $offset + 0x00, 4);
+	my $length =      parse($file_record, $offset + 0x04, 4);
+	my $nonresident = parse($file_record, $offset + 0x08, 1);
+	my $name_length = parse($file_record, $offset + 0x09, 1);
+	my $data_offset = parse($file_record, $offset + 0x14, 2);
 	# Извлекаем данные атрибута
 	my $data = '';
-	if ($resident) {
-		$data = 'unknown';
+	my @dataruns = ();
+	if ($nonresident) {
+		my $dataruns_offset = $offset + $name_length * 2 + 0x40;
+		while (1) {
+			my $fragment_info = parse($file_record, $dataruns_offset, 1);
+			last unless $fragment_info;
+			my $fragment_size_length = $fragment_info & 0x0F;
+			my $fragment_addr_length = $fragment_info >> 4;
+			my $fragmet_size = parse($file_record, $dataruns_offset + 1, $fragment_size_length, 'variable');
+			my $fragmet_addr = parse($file_record, $dataruns_offset + 1 + $fragment_size_length, $fragment_addr_length, 'variable');
+			push @dataruns, $fragmet_addr << 12, ($fragmet_addr + $fragmet_size) << 12;
+			$data .= diskRead( $fragmet_addr << 12 );
+			$dataruns_offset += (1 + $fragment_size_length + $fragment_addr_length);
+		}
 	} else {
-		my $data_length = parse($file_record, $offset + 0x10, 4, "V*");
+		my $data_length = parse($file_record, $offset + 0x10, 4);
 		$data = substr $file_record, $offset + $data_offset, $data_length;
 	}
 	# Парсим содержимое стандартных атрибутов
-	if ($type eq 0x10) {
-		my $ctime = parse($data, 0x00, 8, "I*");
-		# $ctime = localtime($ctime)->strftime('%Y.%m.%d %H:%M:%S');
-		print "== $ctime ==";
-	} elsif ($type eq 0x30) {
-		my $name_length = 2 * parse($data, 0x40, 1, "c*");
-		$data = substr $data, 0x42, $name_length;
-	} elsif ($type eq 0x80) {
+	if ($type == 0x10) {
+		my $ctime = parse($data, 0x00, 8);
+		my $atime = parse($data, 0x08, 8);
+		my $mtime = parse($data, 0x10, 8);
+		my $rtime = parse($data, 0x18, 8);
+		my $flags = parse($data, 0x20, 4, 'flags');
+		$data = "ctime: $ctime\natime: $atime\nmtime: $mtime\nrtime: $rtime\nflags: $flags";
+	} elsif ($type == 0x30) {
+		my $name_length = parse($data, 0x40, 1) * 2;
+		$data =           parse($data, 0x42, $name_length, 'unicode');
+	} else {
+		$data = hexdump($data);
 	}
-	print "[" . HEXED($type) . "]\nresident: $resident\n";
-	print "\n--";
-	print $data;
-	print "--\n\n";
 	$offset += $length;
+	# Добавляем атрибут
+	push @attributes, {
+		type => $type,
+		resident => $nonresident,
+		dataruns => \@dataruns,
+		data => $data
+	};
 }
+
+# Выводим информацию
+DUMP(@attributes);
